ac468c72c7e2e7a1d31c944de2b1006865ac70b3
Nico Schenkel <nico.schenkel612@gmail.com>
2026-06-07 12:09:50 +0200

    manipulability added

--- IRO/Lab/jacobiMatrix.m
+++ IRO/Lab/jacobiMatrix.m
@@ -9,25 +9,22 @@ function [J] = jacobiMatrix(T, TWB)
     % Exctract each T0nj|nj = number of joints|Hence tool is rigid it doesnt have rotation
     for n=1:num_joints
 
-        if n == 1
-            prevRot = TWB(1:3,3);
-            prev_JointLocation = TWB(1:3,4);
         
-        else
-        	prevRot = T{n-1}(1:3,3);
-            prev_JointLocation = T{n-1}(1:3,4);
-        end
-            
-            % v = w x r - durch multiplikation mit qdot wird rotation zu w
+        	prevRot = T{n}(1:3,3);
+            prev_JointLocation = T{n}(1:3,4);
+
+                        % v = w x r - durch multiplikation mit qdot wird rotation zu w
             % Da Matlab von 1 startet immer n-1
             Jv(:,n) = cross(prevRot, position_endeffektor - prev_JointLocation);
             Jw(:,n) = prevRot;
         
-    end
+        end
+            
+
+    
 J = [Jv; Jw];
 
-E = J * J';
-syms lambda
+
 
 end
 
